beb752bb5a463c4d10625376bbee7a6821923168
shinya852 <shinya.june@gmail.com>
2021-11-05 09:26:22 +0000

    コメント機能作成開始

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_062508) do
+ActiveRecord::Schema.define(version: 2021_11_05_092430) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "coment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
