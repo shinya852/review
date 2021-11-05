04f129dfe27ec2e134d3d4b044596790d72b6c50
shinya852 <shinya.june@gmail.com>
2021-11-05 09:07:33 +0000

    画像投稿機能実装完了

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,33 @@
+class PostImagesController < ApplicationController
+
+  def new#新規作成既往
+    @post_image = PostImage.new
+  end
+
+  def create#保存機能
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id#ログイン中のユーザ機能を習得
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'da487829a2ac39d3e685d20f2e4fa302e6c9f0c270edad750348ee226f9e5cf45a8b4f5472d8633d1e72f2c314aa32849fdd61260b9fce8e7d769a911b039a40'
