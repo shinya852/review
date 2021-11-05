a7fa1f5a51df991f25ee901cf2344e32f354558f
shinya852 <shinya.june@gmail.com>
2021-11-05 12:15:47 +0000

    いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do#←親要素の為doを追加
+  #idの受け渡しが必要ない為
+    resource :favorites, only: [:create, :destroy]
   #今回は画像投稿に対してコメントをつけるのでpost_image直下に作成
     resources :post_comments, only: [:create, :destroy]
   end
